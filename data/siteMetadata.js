5a3da8d235b74808fc3689d1a2284e6776ef7ec4
CryptoCanuck <support@rimdc.com>
2026-06-05 22:25:34 -0400

    Disable comments for v1

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -59,7 +59,7 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '', // comments disabled for v1 (set to giscus/utterances/disqus to enable)
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
